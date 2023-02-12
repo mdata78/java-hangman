2e6f4c19a2000cbdadbdc40e824273aeb2e658f5
Iza Klosinska <klosinskaiza@gmail.com>
2023-02-12 10:52:07 +0100

    PutLetter method with scanner added

---
+++ src/main/java/pl/edu/agh/hangman/PutLetter.java
@@ -0,0 +1,18 @@
+package pl.edu.agh.hangman;
+
+import java.util.Scanner;
+
+public class PutLetter {
+
+    public static void addLetter(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide a letter");
+        String letter = scanner.nextLine();
+    }
+
+    public void checkLetter(String letter){
+
+            }
+        }
+    }
+}
